44c18bf20e0300ff2647b7415b552919dfb3abc2
Sharon Watkins <swatkins876@gmail.com>
2019-12-30 15:51:27 -0500

    built gears index route

--- app/controllers/gears_controller.rb
+++ app/controllers/gears_controller.rb
@@ -1,2 +1,32 @@
 class GearsController < ApplicationController
+
+#CREATE
+  #New
+    #Make a ger request '/'
+
+  #Create
+    #make a post request to '/gears'
+
+#READ
+  #Index
+  #Make a get request to '/gears'
+
+  get '/gears' do
+      erb :'gears/index'
+  end
+
+#Show
+  #Make a get request to '/gears/:id'
+
+#UPDATE
+  #Edit
+    #Make a get request to '/gears/:id/edit'
+
+  #Update
+    #Make a patch request to '/gears/:id'
+
+#DESTROY
+  #Make a delete request to '/gears/:id'
+
+
 end
