857f76f4722063906ffe188e0d0e00cb22a35730
Sharon Watkins <swatkins876@gmail.com>
2019-12-31 16:23:38 -0500

    fix controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,5 +16,29 @@ post '/signup' do
   end
 end
 
+get '/login' do
+    erb :'users/login'
+  end
+  
+  post '/login' do
+    if params["username"].empty? || params["password"].empty?
+      @error = "Please supply username and password"
+      erb :'user/login'
+    else
+      if user = User.find_by(username: params["username"], password: params["password"])
+        session[:user_id] = user.id 
+        redirect '/gears'
+      else
+        @error = "User not found"
+        erb :'users/login'
+      end
+  end
+  end
+  
+  get '/logout' do
+      session.clear
+      redirect '/'
+  end
+
 
 end
