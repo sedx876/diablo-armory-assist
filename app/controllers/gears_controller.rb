bece3236f38cbe2770a7c87aa718d8a76937fac6
Sharon Watkins <swatkins876@gmail.com>
2019-12-30 20:30:05 -0500

    edit and delete functions

--- app/controllers/gears_controller.rb
+++ app/controllers/gears_controller.rb
@@ -15,7 +15,7 @@ post '/gears' do
   gear = Gear.new(params)
   if !gear.title.empty?
     gear.save
-    redirect '/gears' 
+    redirect '/gears'
   else
     @error = "To build your Build, You have to at least start with a Build Name"
     erb :'/gears/new'
@@ -34,7 +34,7 @@ end
 #Show
   #Make a get request to '/gears/:id'
 get '/gears/:id' do
-    @gear = Gear.find(params["id"])
+    @gear = Gear.find(params[:id])
     erb :'gears/show'
 end
 
@@ -43,11 +43,33 @@ end
   #Edit
     #Make a get request to '/gears/:id/edit'
 
+get '/gears/:id/edit' do
+  @gear = Gear.find(params[:id])
+  erb :'/gears/edit'
+end
   #Update
     #Make a patch request to '/gears/:id'
 
+patch '/gears/:id' do
+    @gear = Gear.find(params[:id])
+    @gear.update(params["gear"])
+    redirect "/gears/#{params[:id]}"
+#   @gear = Gear.find(params[:id])
+#   if !params["gear"]["title"].empty?
+#     @gear.update(params["gear"])
+#     # gear.update(params["gear"])
+#      redirect "/gears/#{params[:id]}"
+#   else
+#     @error = "Info not Valid, Try again Please!"
+#     erb :'/gears/edit'
+#   end
+end
 #DESTROY
   #Make a delete request to '/gears/:id'
-
+delete '/gears/:id' do
+  gear = Gear.find(params[:id])
+  gear.destroy
+  redirect '/gears'
+  end
 
 end
