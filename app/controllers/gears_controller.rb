c12f7aef2fd5f09faadcba8354649e48a65cbfc4
Sharon Watkins <swatkins876@gmail.com>
2020-01-04 15:33:30 -0500

    indents

--- app/controllers/gears_controller.rb
+++ app/controllers/gears_controller.rb
@@ -4,16 +4,9 @@ before do
   require_login
 end
 
-#CREATE
-  #New
-    #Make a get request '/'
-
 get '/gears/new' do
   erb :'/gears/new'
 end
-
-  #Create
-    #make a post request to '/gears'
 
 post '/gears' do
   gear = current_user.gears.build(params)
@@ -25,17 +18,11 @@ post '/gears' do
 end
 end
 
-#READ
-  #Index
-  #Make a get request to '/gears'
-
 get '/gears' do
-    @gears = Gear.all
-    erb :'gears/index'
+  @gears = Gear.all
+  erb :'gears/index'
 end
 
-#Show
-  #Make a get request to '/gears/:id'
 get '/gears/:id' do
     @gear = Gear.find_by(id: params[:id])
   if @gear
@@ -45,38 +32,21 @@ get '/gears/:id' do
   end
 end
 
-
-#UPDATE
-  #Edit
-    #Make a get request to '/gears/:id/edit'
-
 get '/gears/:id/edit' do
-    @gear = Gear.find(params[:id])
-    erb :'/gears/edit'
+  @gear = Gear.find(params[:id])
+  erb :'/gears/edit'
 end
-  #Update
-    #Make a patch request to '/gears/:id'
 
 patch '/gears/:id' do
-    @gear = Gear.find(params[:id])
-    @gear.update(params["gear"])
-    redirect "/gears/#{params[:id]}"
-#   @gear = Gear.find(params[:id])
-#   if !params["gear"]["title"].empty?
-#     @gear.update(params["gear"])
-#     # gear.update(params["gear"])
-#      redirect "/gears/#{params[:id]}"
-#   else
-#     @error = "Info not Valid, Try again Please!"
-#     erb :'/gears/edit'
-#   end
+  @gear = Gear.find(params[:id])
+  @gear.update(params["gear"])
+  redirect "/gears/#{params[:id]}"
 end
-#DESTROY
-  #Make a delete request to '/gears/:id'
+
 delete '/gears/:id' do
   gear = Gear.find(params[:id])
   gear.destroy
   redirect '/gears'
-  end
+end
 
 end
