32d26f0d6ed050d9420bcc4003ec40ba4ed4cf65
Sharon Watkins <swatkins876@gmail.com>
2020-01-04 10:58:38 -0500

    added spec md fixed indents

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,24 +18,24 @@ end
 
 get '/login' do
     erb :'users/login'
-  end
-  
-  post '/login' do
-    if params["username"].empty? || params["password"].empty?
-      @error = "Please supply username and password"
-      erb :'user/login'
-    else
-      user = User.find_by(:username => params[:username])
+end
+
+post '/login' do
+  if params["username"].empty? || params["password"].empty?
+    @error = "Please supply username and password"
+    erb :'user/login'
+  else
+    user = User.find_by(:username => params[:username])
       if user && user.authenticate(params[:password])
-        session[:user_id] = user.id 
+        session[:user_id] = user.id
         redirect '/gears'
       else
         @error = "User not found"
         erb :'users/login'
       end
   end
-  end
-  
+end
+
 get '/logout' do
   session.clear
   redirect '/'
